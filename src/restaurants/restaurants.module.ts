6824d0b771b562c0ac620a7c78b63d9575489b2b
yeonjuu417 <yeonjuu_clesson@yeonjuui-MacBookPro.local>
2022-03-07 22:35:56 +0900

    ing_typeOrm

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Restaurant } from './entities/restaurant.entity';
 import { RestaurantsResover } from './restaurant.resolver';
+import { RestaurantService } from './restaurants.service';
 
 @Module({
-    providers: [RestaurantsResover]
+    imports: [TypeOrmModule.forFeature([Restaurant])],
+    providers: [RestaurantsResover, RestaurantService]
 })
 export class RestaurantsModule { }
