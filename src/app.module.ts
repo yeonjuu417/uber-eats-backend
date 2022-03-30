47f53dbea8c6031d83a6c2424ef33952972755b4
yeonjuu417 <yeonjuu_clesson@yeonjuui-MacBookPro.local>
2022-03-30 22:04:10 +0900

    reatuarant

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,6 +12,9 @@ import { JwtMiddleware } from './jwt/jwt.middleware';
 import { AuthModule } from './auth/auth.module';
 import { Verification } from './users/entities/verification.entity';
 import { MailModule } from './mail/mail.module';
+import { Restaurant } from './restaurants/entities/restaurant.entity';
+import { Category } from './restaurants/entities/category.entity';
+import { RestaurantsModule } from './restaurants/restaurants.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -47,9 +50,10 @@ import { MailModule } from './mail/mail.module';
       database: process.env.DB_NAME,
       synchronize: process.env.NODE_ENV !== 'prod',
       logging: true,
-      entities: [User, Verification]
+      entities: [User, Verification, Restaurant, Category]
     }),
     UsersModule,
+    RestaurantsModule,
     JwtModule.forRoot({
       privateKey: process.env.PRIVATE_KEY,
     }),
--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,39 +1,41 @@
 import { Field, InputType, ObjectType } from '@nestjs/graphql';
-import { IsBoolean, IsOptional, IsString, Length } from 'class-validator';
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { IsString, Length } from 'class-validator';
+import { CoreEntity } from 'src/common/entities/core.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Column, Entity, ManyToOne } from 'typeorm';
+import { Category } from './category.entity';
 
-@InputType({ isAbstract: true })
+@InputType("RestaurantInputType", { isAbstract: true })
 @ObjectType()
 @Entity()
-export class Restaurant {
-    @PrimaryGeneratedColumn()
-    @Field(type => Number)
-    id: number;
-
+export class Restaurant extends CoreEntity {
     @Field(type => String)
     @Column()
     @IsString()
     @Length(5)
     name: string;
 
-    @Field(type => Boolean, { defaultValue: true, nullable: true })
-    @Column({ default: true })
-    @IsBoolean()
-    @IsOptional()
-    isVegan: boolean;
+    @Field(type => String)
+    @Column()
+    @IsString()
+    coverImg: string;
 
     @Field(type => String, { defaultValue: '강남' })
     @Column()
     @IsString()
     address: string;
 
-    @Field(type => String)
-    @Column()
-    @IsString()
-    ownersName: string;
+    @Field(type => Category, { nullable: true })
+    @ManyToOne(
+        type => Category,
+        category => category.restaurants,
+    )
+    category: Category;
 
-    @Field(type => String)
-    @Column()
-    @IsString()
-    categoryName: string;
+    @Field(type => User)
+    @ManyToOne(
+        type => User,
+        user => user.restaurants,
+    )
+    owner: User;
 }
